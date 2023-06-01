948b88c058a822ccdb811d1e8fb2264b4f3accc2
jlserrerav <122870339+jlserrerav@users.noreply.github.com>
2023-06-01 13:21:58 +0200

    Add files via upload

--- src/es/studium/juego/Ayuda.java
+++ src/es/studium/juego/Ayuda.java
@@ -1,4 +1,4 @@
-package es.studium.juego;
+package es.studium.Poker;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -11,6 +11,7 @@ public class Ayuda  extends Frame {
 
 	private static final long serialVersionUID = 1L;
 	Image imagen;
+	Image siguiente;
 	Image Atras;
 	Toolkit herramienta;
 	Ayuda()
@@ -18,10 +19,11 @@ public class Ayuda  extends Frame {
 		setTitle("Ayuda");
 		setResizable(false);
 		setLocationRelativeTo(null);
-		setSize(900,400);
+		setSize(900,500);
 		setVisible(true);
 		herramienta = getToolkit();
-		imagen = herramienta.getImage("img//fondoAyuda2.jpg");
+		imagen = herramienta.getImage("img//fondoJugar.jpg");
+		siguiente = herramienta.getImage("img//Siguiente.png");
 		Atras = herramienta.getImage("img//atras.png");
 
 	}
@@ -44,20 +46,33 @@ public class Ayuda  extends Frame {
 
 		Font font3 = new Font("Righteous", Font.BOLD, 24);
 		g.setFont(font3);
-		g.drawString("-Puntuajes:", 30, 170);
+		g.drawString("-Puntuajes:", 30, 185);
 		Font font4 = new Font("Righteous", Font.BOLD, 14);
 		g.setFont(font4);
-		g.drawString(" *Carta alta: en caso de empate, la carta más alta gana.", 30, 185);
-		g.drawString(" *Pareja: dos cartas del mismo valor y tres de valores diferentes (en caso de empate, la pareja más alta gana)", 30, 200);
-		g.drawString(" *Doble pareja: dos cartas del mismo valor combinadas con otras dos cartas del mismo valor y una quinta diferente.", 30, 215);
-		g.drawString(" *Trío: tres cartas del mismo valor y dos de un valor diferente ", 30, 230);
-		g.drawString(" *Full house: tres cartas del mismo valor y una pareja.", 30, 245);
-		g.drawString(" *Color: cinco cartas del mismo palo.", 30, 260);
-		g.drawString(" *Póker: cuatro cartas iguales y una diferente.", 30, 275);
-		g.drawString(" *Escalera de color: cinco cartas consecutivas del mismo palo (si hay empate, se reparten las ganancias).", 30, 290);
-		g.drawString(" *Escalera real de color: tener un as, una reina, un rey, una jota y un diez del mismo palo.", 30, 305);
+		g.drawString(" *Carta alta: en caso de empate, la carta más alta gana.", 30, 200);
+		g.drawString(" *Pareja: dos cartas del mismo valor y tres de valores diferentes (en caso de empate, la pareja más alta gana)", 30, 215);
+		g.drawString(" *Doble pareja: dos cartas del mismo valor combinadas con otras dos cartas del mismo valor y una quinta diferente.", 30, 230);
+		g.drawString(" *Trío: tres cartas del mismo valor y dos de un valor diferente ", 30, 245);
+		g.drawString(" *Full house: tres cartas del mismo valor y una pareja.", 30, 260);
+		g.drawString(" *Color: cinco cartas del mismo palo.", 30, 275);
+		g.drawString(" *Póker: cuatro cartas iguales y una diferente.", 30, 290);
+		g.drawString(" *Escalera de color: cinco cartas consecutivas del mismo palo (si hay empate, se reparten las ganancias).", 30, 305);
+		g.drawString(" *Escalera real de color: tener un as, una reina, un rey, una jota y un diez del mismo palo.", 30, 320);
 
-		g.drawImage(Atras, 40, 325, this);
+		g.drawImage(siguiente, 740, 425, this);
+		g.drawImage(Atras, 40, 425, this);
+		Color color2 = new Color(229, 74, 53);
+
+		g.setColor(color2);
+
+		g.drawRect(40, 425, 126, 50);
+
+		Color color3 = new Color(229, 74, 53);
+
+		g.setColor(color3);
+
+		g.drawRect(740, 425, 126, 50);
+		
 
 	}
 }
