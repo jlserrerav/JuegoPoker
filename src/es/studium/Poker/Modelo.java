3fc9d33580e8b3da1f980ae586b34788897a45d2
jlserrerav <122870339+jlserrerav@users.noreply.github.com>
2023-06-12 13:45:53 +0200

    Update Modelo.java

--- src/es/studium/Poker/Modelo.java
+++ src/es/studium/Poker/Modelo.java
@@ -5,25 +5,17 @@ import java.util.Random;
 public class Modelo
 {
 	Random rnd = new Random();
-	public void barajarCentro(int uno[], int dos[],int tres[], int cuatro[],int cinco[])
-	{		
-		int jugador = 0;
-		
-		/* 
-		 * 
-		 * c1=0, c2=0
-		 * while (c1 == c2 && c1 == c3 && c1 = c4){
-		 * c1 = new randomm
-		 * c2 = new random
-		 * 
-		 * */
-		int iUno=0, iDos=0, iTres=0, iCuatro=0, iCinco=0;
-		
+	public void barajarCentro1(int uno[], int dos[])
+	{
+		int baraja = 0;
+
+		int iUno=0, iDos=0;
+
 		for(int i = 1; i <= 52; i++)
 		{
-			jugador = rnd.nextInt(2);
-			
-			if((jugador==0)&&(iUno<26)) // Carta Centro 1
+			baraja = rnd.nextInt(2);
+
+			if((baraja==0)&&(iUno<26)) // Carta Centro 1
 			{
 				uno[iUno] = i;
 				iUno++;
@@ -33,43 +25,57 @@ public class Modelo
 				dos[iDos] = i;
 				iDos++;
 			}
-			else if(iTres<26) // Carta Centro 3
+		}
+		rebarajar(uno);
+		rebarajar(dos);
+
+	}
+	public void barajarCentro2(int tres[], int cuatro[], int cinco[])
+	{
+		int baraja = 0;
+
+		int iTres=0, iCuatro=0, iCinco=0;
+
+		for(int i = 1; i <= 52; i++)
+		{
+
+
+			if((baraja==0)&&(iTres<16)) // Carta Centro 3
 			{
 				tres[iTres] = i;
 				iTres++;
 			}
-			else if(iCuatro<26) // Carta Centro 4
+			else if(iCuatro<16) // Carta Centro 4
 			{
 				cuatro[iCuatro] = i;
 				iCuatro++;
 			}
-			else if(iCinco<26) // Carta Centro 5
+			else if(iCinco<16) // Carta Centro 5
 			{
 				cinco[iCinco] = i;
 				iCinco++;
 			}
-			
 		}
-		rebarajar(uno);
-		rebarajar(dos);
 		rebarajar(tres);
 		rebarajar(cuatro);
 		rebarajar(cinco);
+
+
 	}
-	public void barajar(int uno[], int dos[])
-	{		
+	public void barajarJugador1(int uno[], int dos[])
+	{
 		int jugador = 0;
 		int iUno=0, iDos=0;
-		
+
 		for(int i = 1; i <= 52; i++)
 		{
 			jugador = rnd.nextInt(2);
-			if((jugador==0)&&(iUno<26)) // Jugador 1
+			if((jugador==0)&&(iUno<26)) // Jugador 1 1ºCarta
 			{
 				uno[iUno] = i;
 				iUno++;
 			}
-			else if(iDos<26) // Jugador 2
+			else if(iDos<26) // Jugador 1 2ºCarta
 			{
 				dos[iDos] = i;
 				iDos++;
@@ -78,13 +84,35 @@ public class Modelo
 		rebarajar(uno);
 		rebarajar(dos);
 	}
-	
+	public void barajarJugador2(int uno[], int dos[])
+	{
+		int jugador = 0;
+		int iUno=0, iDos=0;
+
+		for(int i = 1; i <= 52; i++)
+		{
+			jugador = rnd.nextInt(2);
+			if((jugador==0)&&(iUno<26)) // Jugador 1 1ºCarta
+			{
+				uno[iUno] = i;
+				iUno++;
+			}
+			else if(iDos<26) // Jugador 1 2ºCarta
+			{
+				dos[iDos] = i;
+				iDos++;
+			}
+		}
+		rebarajar(uno);
+		rebarajar(dos);
+	}
+
 	public void rebarajar(int t[])
 	{
 		int aleatorio, auxiliar;
-		for(int i=0; i<26; i++)
+		for(int i=0; i<16; i++)
 		{
-			aleatorio = rnd.nextInt(25)+1;
+			aleatorio = rnd.nextInt(15)+1;
 			auxiliar = t[i];
 			t[i] = t[aleatorio];
 			t[aleatorio] = auxiliar;
