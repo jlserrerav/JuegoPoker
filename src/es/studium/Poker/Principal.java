18f6660b4341e385202a9668938100cb2db8f70a
jlserrerav <122870339+jlserrerav@users.noreply.github.com>
2023-06-12 13:46:15 +0200

    Principal.java

--- src/es/studium/Poker/Principal.java
+++ src/es/studium/Poker/Principal.java
@@ -4,10 +4,6 @@ public class Principal {
 
 	public static void main(String[] args)
 	{
-		Modelo modelo = new Modelo();
-		MenuPrincipal menuPrincipal = new MenuPrincipal();
-		Juego juego = new Juego();
-		new Controlador(modelo, menuPrincipal);
-		new ControladorJuego(juego, modelo);
+		new Controlador(new Juego(), new Modelo(), new MenuPrincipal());
 	}
 }
